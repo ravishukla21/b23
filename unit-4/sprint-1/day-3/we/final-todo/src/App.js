884323c50c05c88d614dd350f46566b75f709e54
ravi s <ravi21.434@gmail.com>
2023-01-29 11:02:39 +0530

    final-todo

--- unit-4/sprint-1/day-3/we/final-todo/src/App.js
+++ unit-4/sprint-1/day-3/we/final-todo/src/App.js
@@ -1,11 +1,35 @@
 // import logo from './logo.svg';
+import { useState } from "react"
 import './App.css';
 
 function App() {
+  const [text, setText] = useState("")
+  const [handleclick, sethandleclick] = useState(["abra"])
+
+  let hello123 = (e) => { setText(e.target.value) }
+  let onClick = () => {
+
+    let newarray=[...handleclick,text]
+
+    sethandleclick(newarray)
+    setText("")
+   // console.log(text)
+    console.log(handleclick,"tiy")
+  
+  }
+  console.log(handleclick,"hi")
+
+  // let jigra=handleclick.map((el)=>{return <h1>{el}</h1>})
   return (
+
+   
     <div className="App">
-     hello123
-     
+      <input onChange={hello123} value={text} />
+      <button onClick={onClick} >ADD</button>
+      <div>
+        {/* {jigra} */}
+      </div>
+
     </div>
   );
 }
