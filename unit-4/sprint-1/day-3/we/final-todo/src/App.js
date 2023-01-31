c4a1c5452f62683edf6a2fe9a5aee11bdf5db528
ravi s <ravi21.434@gmail.com>
2023-01-31 08:21:14 +0530

    hello

--- unit-4/sprint-1/day-3/we/final-todo/src/App.js
+++ unit-4/sprint-1/day-3/we/final-todo/src/App.js
@@ -4,31 +4,43 @@ import './App.css';
 
 function App() {
   const [text, setText] = useState("")
-  const [handleclick, sethandleclick] = useState(["abra"])
+  const [handleclick, sethandleclick] = useState([])
 
   let hello123 = (e) => { setText(e.target.value) }
   let onClick = () => {
 
-    let newarray=[...handleclick,text]
-
-    sethandleclick(newarray)
+    const newItem = {
+      id: Date.now() + Math.random() + text,
+      title: text,
+      status: false,
+    };
+    const todoListAfterItemAddition = [...handleclick, newItem];
+    sethandleclick(todoListAfterItemAddition);
     setText("")
-    console.log(text)
-    console.log(handleclick,"tiy")
-  
-  }
-  console.log(handleclick,"hi")
 
-  let jigra=handleclick.map((el)=>{return <h1>{el}</h1>})
+    // console.log(handleclick,"tiy")
+
+  }
+  console.log(text)
+  console.log(handleclick, "hi")
+
+  let jigra = handleclick.map((el) => { return el.title })
+  console.log(jigra,"hjkl")
+
   return (
 
-   
+
     <div className="App">
       <input onChange={hello123} value={text} />
       <button onClick={onClick} >ADD</button>
-      <div>
-        {jigra}
-      </div>
+      {handleclick.map((todo) => {
+                    return (
+                        <div key={todo.id}>
+                            {todo.title}-{todo.status ? "Completed" : "Not Completed"}-{"Delete"}
+
+                        </div>
+                    )
+                })}
 
     </div>
   );
