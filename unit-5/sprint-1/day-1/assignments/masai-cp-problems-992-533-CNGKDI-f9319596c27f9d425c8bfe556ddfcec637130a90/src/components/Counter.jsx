6832c60e6f3c856cdd8b998bda4894e80569baf0
ravishukla21 <ravi21.434@gmail.com>
2023-03-01 21:25:49 +0530

    hi

--- unit-5/sprint-1/day-1/assignments/masai-cp-problems-992-533-CNGKDI-f9319596c27f9d425c8bfe556ddfcec637130a90/src/components/Counter.jsx
+++ unit-5/sprint-1/day-1/assignments/masai-cp-problems-992-533-CNGKDI-f9319596c27f9d425c8bfe556ddfcec637130a90/src/components/Counter.jsx
@@ -1,28 +1,46 @@
 import {useState} from "react";
+import React from "react";
 
 
 export default function Counter(props){
-    const [count,setcount] =useState(0);
-    //console.log(props,"props")
-    const handleClick=(count,val)=>{
-        console.log(count,val,"val")
-        console.log(count+val)
-        setcount(count+val)
 
+    const [initCount,setinitCount] = useState(0);
+
+    //console.log(props,"props")
+
+    // const handleClick=(count,val)=>{
+    //     console.log(count,val,"val")
+    //     console.log(count+val)
+    //     setcount(count+val)
+
+
+    // }
+    // const handlemul=(count,val)=>{
+    //     setcount(count*val)
+    // }
+
+    const handleAdd=(initCount,val)=>{
+        setinitCount(initCount+val)
 
     }
-    const handlemul=(count,val)=>{
-        setcount(count*val)
+    const handleSub=(initCount,val)=>{
+        setinitCount(initCount+val)
+
+    }
+    const handlemul=(initCount,val)=>{
+        setinitCount(initCount*val)
+
     }
 
     return(
         <div>
+            <h2 data-testid="counter-header">Counter</h2>
              
-            <h3 data-testid="counter-header" >{count}</h3>
+            <h3 data-testid="count" >{initCount}</h3>
            
-            <button  textContent="+" data-testid="add-btn" onClick={()=>{handleClick(count,+1)}}>+</button>
-            <button textContent="-"  data-testid="subtract-btn"  onClick={()=>{handleClick(count,-1)}}>-</button>
-            <button data-testid="double-btn"  onClick={()=>{handlemul(count,2)}}>Double</button>
+            <button   data-testid="add-btn" onClick={()=>{handleAdd(initCount,+1)}}>+</button>
+            <button  data-testid="subtract-btn"  onClick={()=>{handleSub(initCount,-1)}}>-</button>
+            <button data-testid="double-btn"  onClick={()=>{handlemul(initCount,2)}}>Double</button>
 
         </div>
 
