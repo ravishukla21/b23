eed6e241ca1b72f96505070f30023514e831bba9
ravishukla21 <ravi21.434@gmail.com>
2023-03-24 08:40:45 +0530

    type

---
+++ unit-5/sprint-4/day-1/assignments/typescript/src/index.ts
@@ -0,0 +1,43 @@
+let arr=[1,2,3]
+let arr12=[4,5,6]
+let arr78=[5,6,7]
+let arr128="amitabh";
+
+const art:boolean=true;
+ const helloWorld:string="ravi";
+ const hiop:string="kite";
+ 
+ interface user{
+    name:string;
+    id:number;
+ }
+
+ const user= {
+    name:"ravi",
+    id:0
+ }
+
+interface amit{
+    name:string;
+    id:Number;
+}
+
+const user12:amit={
+    name:"ravi",
+    id:0
+
+}
+
+
+interface amitabhsrivastava{
+    name:number;
+    irname:string;
+}
+
+const ok12:amitabhsrivastava={
+    name:45,
+    irname:"ravisshukla"
+}
+
+
+const hek=[1,2,3]
