e20f79c7e8c7004ebb52bdc28e7e3e0e1ebcbf39
ravishukla21 <ravi21.434@gmail.com>
2023-03-24 11:55:44 +0530

    24march

---
+++ unit-5/sprint-4/day-2/assignments/masai-cp-problems-1208-1616-NgboTq-00a87a955fce3ad7e2a0e6628129174f146310e4/src/App.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+  
+  const [counter,setcounter]=useState<number>(0);
+  const handleClick=(e:React.MouseEvent<HTMLButtonElement>)=>{
+    console.log(e,"e")
+    setcounter(counter+1)
+    
+   console.log("hello",counter);
+  }
+  const handleClick1=(e:React.MouseEvent<HTMLButtonElement>)=>{
+    setcounter(counter-1)
+
+  }
+  const handleClick2=(e:React.MouseEvent<HTMLButtonElement> )=>{
+    setcounter(counter+5)
+
+
+  }
+  const handleClick3=(e:React.MouseEvent<HTMLButtonElement>)=>{
+    setcounter(counter-5)
+  }
+
+  useEffect(()=>{
+   const obj={
+      counter
+    }
+    console.log(obj)
+    localStorage.setItem("counter",JSON.stringify(obj.counter));
+  },[counter]);
+
+  return (
+    <div className="App">
+      <h1 data-testid="counter">Count:{counter}</h1>
+      <button data-testid="increment" onClick={handleClick}>Increment</button>
+      <button data-testid="decrement" onClick={handleClick1}>Decrement</button>
+      <button data-testid="increment5" onClick={handleClick2}>Increment 5 times</button>
+      <button data-testid="decrement5" onClick={handleClick3} >Decrement 5 times</button>
+    </div>
+  );
+}
+
+export default App;
