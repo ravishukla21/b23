cd06c41ce0938cc7b4b2016bde7046804cbe32cd
ravi shukla <ravi21.434@gmail.com>
2023-04-16 16:22:51 +0530

    cartreducerchecked

--- unit-5/sprint-2/evaluation/prev/masai-cp-problems-1113-1613-ZIqMkd-83741b5a3b24ac5723ec0f6612d75f35b63e1841/src/Components/ProductCard.jsx
+++ unit-5/sprint-2/evaluation/prev/masai-cp-problems-1113-1613-ZIqMkd-83741b5a3b24ac5723ec0f6612d75f35b63e1841/src/Components/ProductCard.jsx
@@ -1,7 +1,18 @@
-export const ProductCard = ({title,discountPercentage,brand
-,price,thumbnail,id
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart } from "../Redux/CartReducer/action";
+
+export const ProductCard = ({ title, discountPercentage, brand
+  , price, thumbnail, id
 
 }) => {
+  const dispatch = useDispatch();
+ const store= useSelector((store)=>store.cartReducer)
+ console.log(store.cart,"cartreducer")
+
+  const handleclick = (id) => {
+    console.log(id, "id")
+    dispatch(addToCart(id))
+  }
   return (
     <div className="product-card">
       {/* - Every product card will have a `add to cart` button with class `add-to-cart`
@@ -12,18 +23,18 @@ export const ProductCard = ({title,discountPercentage,brand
        - Show discount in p tag with class `product-discount` 
        * Do not add any extra text, only response values should be present * 
        * For example `Title:"iPhone"` or `Discount: 10.5%` will not work * */}
-       <div>
-         <img className="product-image" style={{width:"100%"}} src={thumbnail} ></img>
-         <p className="product-title">{title}</p>
-         <p className="product-brand">{brand}</p>
-         <p className="product-price">{price}</p>
-         <p className="product-discount">{discountPercentage}</p>
-         <button className="add-to-cart">Add to Cart</button>
+      <div>
+        <img className="product-image" style={{ width: "100%" }} src={thumbnail} ></img>
+        <p className="product-title">{title}</p>
+        <p className="product-brand">{brand}</p>
+        <p className="product-price">{price}</p>
+        <p className="product-discount">{discountPercentage}</p>
+        <button className="add-to-cart" onClick={() => handleclick(id)} >Add to Cart</button>
 
 
-       
-       
-       </div>
+
+
+      </div>
     </div>
   );
 };
--- unit-5/sprint-2/evaluation/prev/masai-cp-problems-1113-1613-ZIqMkd-83741b5a3b24ac5723ec0f6612d75f35b63e1841/src/Redux/CartReducer/action.js
+++ unit-5/sprint-2/evaluation/prev/masai-cp-problems-1113-1613-ZIqMkd-83741b5a3b24ac5723ec0f6612d75f35b63e1841/src/Redux/CartReducer/action.js
@@ -1,9 +1,16 @@
+import axios from "axios";
 import { ADD_TO_CART } from "./actionTypes";
 
 
-export const addToCart = () => {
+export const addToCart = (id)=>(dispatch) => {
   // Complete add to cart store functionality
+  axios.get(`http://localhost:${process.env.REACT_APP_JSON_SERVER_PORT}/products/${id}`).then((res)=>
+
+  {dispatch({type:ADD_TO_CART,payload:res.data})}
+  )
 };
+
+
 
 // NOTE: Do not remove this code,its used for calculating your score, if removed it will give you zero marks
 if (window.Cypress) {
