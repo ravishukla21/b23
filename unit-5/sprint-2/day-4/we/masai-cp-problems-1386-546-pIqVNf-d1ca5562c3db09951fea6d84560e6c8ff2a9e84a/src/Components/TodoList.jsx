d07490cacfd7340774cc7f573196f9c66435b5a1
ravi shukla <ravi21.434@gmail.com>
2023-04-16 08:02:39 +0530

    hhhhhhhhh

--- unit-5/sprint-2/day-4/we/masai-cp-problems-1386-546-pIqVNf-d1ca5562c3db09951fea6d84560e6c8ff2a9e84a/src/Components/TodoList.jsx
+++ unit-5/sprint-2/day-4/we/masai-cp-problems-1386-546-pIqVNf-d1ca5562c3db09951fea6d84560e6c8ff2a9e84a/src/Components/TodoList.jsx
@@ -8,20 +8,21 @@ const TodoList = () => {
   console.log(store, "store")
   const { isError, isLoading, todos } = store;
   console.log(isError, isLoading, todos, "todosiserrorisloading")   
+  console.log(todos.length,"length")
   //  {todos?.map((el,index)=><div key={index}> <h1>{el.title}--{el.status?"True":"False"}</h1> </div>)}
   return (
     <div data-testid='todo-list-wrapper'>
 
       {/* map all the "individual/single" todo inside this div. */}
-      <div data-testid="single-todo" >
-      {isLoading?"...loading":todos?.map((el,index)=><div  key={index}> <h1>{el.title}--{el.status?"True":"False"}</h1> </div>)}
+     
+      {isLoading?"...loading":todos?.map((el,index)=><div data-testid="single-todo" key={index}> <h1>{el.title}--{el.status?"True":"False"}</h1> </div>)}
       
 
      
 
 
 
-      </div>
+ 
 
 
     </div>
