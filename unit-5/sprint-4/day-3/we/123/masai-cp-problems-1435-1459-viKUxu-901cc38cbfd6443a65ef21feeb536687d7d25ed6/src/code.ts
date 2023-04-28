ee49713441137defbe8806cb85017508020e6b5a
ravi shukla <ravi21.434@gmail.com>
2023-04-28 18:21:10 +0530

    interface

---
+++ unit-5/sprint-4/day-3/we/123/masai-cp-problems-1435-1459-viKUxu-901cc38cbfd6443a65ef21feeb536687d7d25ed6/src/code.ts
@@ -0,0 +1,13 @@
+interface test{
+    title:string;
+    status:boolean;
+    id:number;
+}
+
+let Obj:test={
+    title:"React",
+    status:true,
+    id:4
+}// This Obj variable should have the interface that you will create
+
+export default Obj;// Make no changes here
