4f6c43853f2e1d18721055114c15088288af7f1a
ravi shukla <ravi21.434@gmail.com>
2023-05-11 17:48:41 +0530

    calcal

--- unit-6/sprint-1/day-2/assignments/masai-cp-problems-1524-1504-bREYIM-4bcfdff5fcacf2df8ca5061bf2b139224a5a81d7/index.js
+++ unit-6/sprint-1/day-2/assignments/masai-cp-problems-1524-1504-bREYIM-4bcfdff5fcacf2df8ca5061bf2b139224a5a81d7/index.js
@@ -2,13 +2,14 @@
 
 //  import the crypto module
 
-const crypto=require("crypto")
+const { clear } = require("console")
+const crypto = require("crypto")
 
 
 //  get a commands using process.argv
-const operation=process.argv[2]
-const num1=+process.argv[3]
-const num2=+process.argv[4]
+const operation = process.argv[2]
+const num1 = +process.argv[3]
+const num2 = +process.argv[4]
 
 // console.log(num1+num2,"nan")
 
@@ -17,16 +18,16 @@ const num2=+process.argv[4]
 
 
 
-const Calculate=()=>{
+const Calculate = () => {
   switch (operation) {
     case "add":
-      return num1+num2
+      return num1 + num2
     case "sub":
-      return num1-num2
+      return num1 - num2
     case "mult":
-      return num1*num2
+      return num1 * num2
     case "divide":
-      return num1/num2;
+      return num1 / num2;
     case "sin":
       return Math.sin(num1);
     case "cos":
@@ -34,20 +35,22 @@ const Calculate=()=>{
     case "tan":
       return Math.tan(num1);
     case "random":
-      if(!num1){
+      if (!num1) {
         return "Provide length for random number generation."
 
-      }else{
-        const val=crypto.randomBytes(num1)
-        return val.toString("bi")
+      } else {
+        const val = crypto.randomBytes(num1)
+        return val.toString("binary")
+        // Calling randomBytes method with callback
+      
 
       }
-    
-    
+
+
     default:
       console.log("Invalid operation");
   }
-  
+
 }
 
 console.log(Calculate());
