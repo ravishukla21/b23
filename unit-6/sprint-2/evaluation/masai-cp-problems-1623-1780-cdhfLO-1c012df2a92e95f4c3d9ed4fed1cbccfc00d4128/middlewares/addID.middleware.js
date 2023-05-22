a436e04b762d8c7f8f8cc57d92c4053df3fa8304
ravi shukla <ravi21.434@gmail.com>
2023-05-22 16:30:27 +0530

    addid

--- unit-6/sprint-2/evaluation/masai-cp-problems-1623-1780-cdhfLO-1c012df2a92e95f4c3d9ed4fed1cbccfc00d4128/middlewares/addID.middleware.js
+++ unit-6/sprint-2/evaluation/masai-cp-problems-1623-1780-cdhfLO-1c012df2a92e95f4c3d9ed4fed1cbccfc00d4128/middlewares/addID.middleware.js
@@ -3,7 +3,10 @@ const addID = (req,res,next) => {
   const data=JSON.parse(fs.readFileSync("./db.json","utf-8"));
   
   console.log(data.heroes.length,"datainid")
-  let length=data.heroes.length
+  const last=data.heroes[data.heroes.length-1]
+  const newID=last? last.id+1 :1
+ 
+  req.body.id=newID
 
  next();
 
